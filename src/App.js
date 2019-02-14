59eb21e7273846c2c23012b86d6d4d629d57a7b7
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2019-02-14 22:16:21 +0100

    Add base map to cra

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import SdkMap from '@boundlessgeo/sdk/components/map';
+import SdkMapReducer from '@boundlessgeo/sdk/reducers/map';
+import * as SdkMapActions from '@boundlessgeo/sdk/actions/map';
+
+const store = createStore(combineReducers({
+  'map': SdkMapReducer,
+}));
 
 class App extends Component {
+
+  componentDidMount() {
+	  // add the OSM source
+	  store.dispatch(SdkMapActions.addSource('osm', {
+		type: 'raster',
+		tileSize: 256,
+		tiles: [
+		  'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		  'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		  'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		],
+	  }));
+
+	  // add an OSM layer
+	  store.dispatch(SdkMapActions.addLayer({
+		id: 'osm',
+		source: 'osm',
+	  }));
+	}
+
   render() {
     return (
       <div className="App">
@@ -20,6 +49,11 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div>
+			    <Provider store={store}>
+            <SdkMap />
+          </Provider>
+		    </div>
       </div>
     );
   }
