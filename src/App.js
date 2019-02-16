8c47b3a6bba49ed5f287240bf49da50decbe088b
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2019-02-16 15:11:03 +0100

    Add popup content on click

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SdkMap from '@boundlessgeo/sdk/components/map';
 import SdkZoomControl from '@boundlessgeo/sdk/components/map/zoom-control';
 import SdkZoomSlider from '@boundlessgeo/sdk/components/map/zoom-slider';
 import SdkMapReducer from '@boundlessgeo/sdk/reducers/map';
+import SdkPopup from '@boundlessgeo/sdk/components/map/popup';
 import * as SdkMapActions from '@boundlessgeo/sdk/actions/map';
 
 import '@boundlessgeo/sdk/stylesheet/sdk.scss';
@@ -15,10 +16,35 @@ const store = createStore(combineReducers({
   'map': SdkMapReducer,
 }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+/** A popup for marking features when they
+ *  are selected.
+ */
+class FeaturesPopup extends SdkPopup {
+
+  render() {
+    const feature_names = this.props.features.map(f => f.properties.name);
+
+    return this.renderPopup((
+      <div className='sdk-popup-content'>
+        You clicked here:<br />
+        <code>
+          { this.props.coordinate.hms }
+        </code>
+        <br />
+        <p>
+          Feature NAME(s):<br />
+          <code>{ feature_names }</code>
+          <br />
+        </p>
+      </div>
+    ));
+  }
+}
+
 class App extends Component {
 
   componentDidMount() {
-    // start in the middle of america
+    // start in the middle of italy
     store.dispatch(SdkMapActions.setView([12, 40], 4));
 
 	  // add the OSM source
@@ -75,7 +101,36 @@ class App extends Component {
         </header>
         <div>
 			    <Provider store={store}>
-            <SdkMap>
+            <SdkMap
+              includeFeaturesOnClick
+              onClick={(map, xy, featuresPromise) => {
+                featuresPromise.then((featureGroups) => {
+                  // setup an array for all the features returned in the promise.
+                  let features = [];
+
+                  // featureGroups is an array of objects. The key of each object
+                  // is a layer from the map.
+                  for (let g = 0, gg = featureGroups.length; g < gg; g++) {
+                    // collect every feature from each layer.
+                    const layers = Object.keys(featureGroups[g]);
+                    for (let l = 0, ll = layers.length; l < ll; l++) {
+                      const layer = layers[l];
+                      features = features.concat(featureGroups[g][layer]);
+                    }
+                  }
+
+                  if (features.length === 0) {
+                    // no features, :( Let the user know nothing was there.
+                    map.addPopup(<SdkPopup coordinate={xy} closeable><i>No features found.</i></SdkPopup>);
+                  } else {
+                    // Show the super advanced fun popup!
+                    map.addPopup(<FeaturesPopup coordinate={xy} features={features} closeable />);
+                  }
+                }).catch((exception) => {
+                  console.error('An error occurred.', exception);
+                });
+              }}
+            >
               <SdkZoomControl style={{position: 'absolute', top: 10, left: 14}} />
               <SdkZoomSlider />
             </SdkMap>
